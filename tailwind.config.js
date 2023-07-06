60d384f7fd2be12c8943bd147612c05704c96a44
Soumyadeep Manna <soumyadeepm93@gmail.com>
2023-07-06 15:57:53 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
